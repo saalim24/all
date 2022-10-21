b10d6ca09a471f7c91288e87b47769819f929bd1
saalim <saalim520@gmail.com>
2022-10-22 00:06:47 +0530

    OK

--- Python/a.py
+++ Python/a.py
@@ -1,1 +1,10 @@
+import time 
+import pyautogui
+time.sleep(5)
+for i in range (10):
+    pyautogui.typewrite("""hello""")
+    pyautogui.press("space")
+
+
+
 
--- online.cpp/a.c
+++ online.cpp/a.c
@@ -1,31 +1,39 @@
+// A simple C program that traverses and prints the elements of doubly linked list
 #include<stdio.h>
-#include<stdlib.h>
-struct Node
-{
+struct Node{                 
     int data;
-    struct Node*next;
+    struct Node* next;
+    struct Node* prev;
 };
 
-void create(int arr[],int n)
+void traverse (struct Node*head)
 {
-    struct Node*first = (struct Node*)malloc(sizeof(struct Node));
-    first->data=arr[0];
-    first->next=NULL;
-    struct Node*last=first;
-
-    for (int i=1;i<n;i++)
+    struct Node*curr=head;
+    while (curr->next!=NULL)
     {
-        struct Node*t= (struct Node*)malloc(sizeof(struct Node));
-        t->data=arr[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
+        printf("%d ",curr->data);
+        curr=curr->next;
     }
+    printf("%d \n",curr->data);
 
 }
-
 int main()
 {
-    struct Node*temp;
-}
+    struct Node*head=NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+  
+    head->data = 1; // assign data in first Node
+    head->next = second; // Link first Node with second
+  
+    second->data = 2; // assign data to second Node
+    second->next = third;
+  
+    third->data = 3; // assign data to third Node
+    third->next = NULL;
+    traverse(head);
 
+}
